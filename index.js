d0b7094efadd0f29ba2c6b5502a7b4982dcedac3
alain-cheng <104754302+alain-cheng@users.noreply.github.com>
2022-05-02 19:05:26 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var Post = function(thumbnail, firstName, lastName, course, term, degree, college) {
+                this.thumbnail = thumbnail;
+                this.firstName = firstName;
+                this.lastName  = lastName;
+                this.course    = course;
+                this.term      = term;
+                this.degree    = degree;
+                this.college   = college;
+           }
+  
