033dc0e0a73833c7ecef758980fcc96df915a1f7
ExME168 <addisonfadrigo@gmail.com>
2022-05-03 23:39:05 +0800

    navbar and write-a-review divs added

--- index.js
+++ index.js
@@ -7,4 +7,27 @@ var Post = function(thumbnail, firstName, lastName, course, term, degree, colleg
                 this.degree    = degree;
                 this.college   = college;
            }
+
+$(document).ready(function () {
+
+     $('.navbar-buttons').hover(function() {
+          $(this).css("opacity", "50%");
+     }, function (){
+          $(this).css("opacity", "100%");
+     });
+
+     $('.navbar-loginregister').hover(function() {
+          $(this).css("opacity", "50%");
+     }, function (){
+          $(this).css("opacity", "100%");
+     });
+
+     $('.reviewSubmit').hover(function() {
+          $(this).css("opacity", "50%");
+     }, function (){
+          $(this).css("opacity", "100%");
+     });
+          
+     
+});
   
