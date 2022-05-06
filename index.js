c4aefdd023b83ba0c6b3eb980c42ee977e267923
akn5 <104754302+alain-cheng@users.noreply.github.com>
2022-05-06 09:28:50 +0800

    Added user objects and post object edits
    5 sample users and 5 sample posts owned by different users have been created.
    Also edits to the Post object by adding an owner variable + removal of some variables that can go to the owner var.
    Additional, created global variables for storing all users and posts

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-var Post = function(thumbnail, firstName, lastName, course, term, degree, college, stars) {
+var User = function(firstName, lastName, degree, college, username, password, picture) {
+               this.firstName = firstName;
+               this.lastName  = lastName;
+               this.degree    = degree;
+               this.college   = college;
+               this.username  = username;
+               this.password  = password;
+               this.picture   = picture; // save as string
+          }
+
+
+var Post = function(thumbnail, course, term, stars, owner) {
                 this.thumbnail = thumbnail;
-                this.firstName = firstName;
-                this.lastName  = lastName;
                 this.course    = course;
                 this.term      = term;
-                this.degree    = degree;
-                this.college   = college;
                 this.stars     = stars;
+                this.owner     = owner; // we can assign nalang the firstName, lastName, degree, and college of the post owner here
            }
+
+var users = [] // save all users here
+var posts = [] // all posts
 
 $(document).ready(function () {
 
+     // generate 5 sample users
+     var user1 = new User("John", "Doe", "BSCS", "College of Science", "JDoe", "user1", ".public/empty-profile-pic.jpeg");
+     var user2 = new User("Maria", "Christina", "BSCS", "College of Science", "Maria", "user2", ".public/empty-profile-pic.jpeg");
+     var user3 = new User("Amy", "Rose", "BSCS", "College of Science", "Roseamy", "user3", ".public/empty-profile-pic.jpeg");
+     var user4 = new User("Lance", "Mendoza", "BSCS", "College of Science", "LanDoza", "user4", ".public/empty-profile-pic.jpeg");
+     var user5 = new User("Francis", "Brown", "BSCS", "College of Science", "Francy", "user5", ".public/empty-profile-pic.jpeg");
+     users.push(user1, user2, user3, user4, user5);
+
+     // generate 5 sample posts owned by user2 to user5
+     var post1 = new Post("./public/placeholder-thumbnail.jpeg", "CCPROG", 2, 5, user2);
+     var post2 = new Post("./public/placeholder-thumbnail.jpeg", "CCDSTRU", 1, 4, user3);
+     var post3 = new Post("./public/placeholder-thumbnail.jpeg", "CCPROG2", 2, 3, user3);
+     var post4 = new Post("./public/placeholder-thumbnail.jpeg", "CCPROG", 1, 4, user4);
+     var post5 = new Post("./public/placeholder-thumbnail.jpeg", "CSINTSY", 3, 2, user5);
+     posts.push(post1, post2, post3, post4, post5);
+
+     // debugging
+     console.log("post1 course: " + post1.course);
+     console.log("post1 is owned by: " + post1.owner.username);
+     console.log("post5 is owned by: " + post5.owner.username);
+     
      let rating;
 
      $(".loginContainer").css("visibility", "hidden");
